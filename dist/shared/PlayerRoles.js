51afe79e0c2fabf77d6c52048a0faa0d6e10b156
Yuri Poluk <polukyu@gmail.com>
2020-08-11 19:34:07 +0300

    new build script for heroku

--- dist/shared/PlayerRoles.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PlayerRoles = void 0;
-var PlayerRoles;
-(function (PlayerRoles) {
-    PlayerRoles[PlayerRoles["Player1"] = 0] = "Player1";
-    PlayerRoles[PlayerRoles["Player2"] = 1] = "Player2";
-})(PlayerRoles = exports.PlayerRoles || (exports.PlayerRoles = {}));
-//# sourceMappingURL=PlayerRoles.js.map
